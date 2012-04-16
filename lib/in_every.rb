23e87ecc0e16d30465a41d69d94f6d1181a5cd9f
Colin Gemmell <pythonandchips@gmail.com>
2012-04-16 20:17:55 +0100

    made generate occurrencies private. no need for it to be public

--- lib/in_every.rb
+++ lib/in_every.rb
@@ -3,15 +3,10 @@ require 'in_every/enumerator_extensions'
 class InEvery
   class SetupException < Exception; end
 
-  def initialize enum, executions
+  def initialize(enum, executions)
     @executions = (executions - 1)
     @enum = enum
     generate_occurances
-  end
-
-  def generate_occurances
-    raise SetupException.new("occurancies must be greater than executions") if @enum.max > @executions
-    @failures = (0..(@executions)).to_a.sample(@enum.max).sort
   end
 
   def start(&block)
@@ -21,7 +16,7 @@ class InEvery
   def execute *args
     @counter ||= 0
     if @counter == @failures.first
-      @method.call(*args)
+      result = @method.call(*args)
       @failures.delete_at(0)
     end
     if @counter == @executions
@@ -30,5 +25,13 @@ class InEvery
     else
       @counter += 1
     end
+    result
+  end
+
+  private
+
+  def generate_occurances
+    raise SetupException.new("occurancies must be greater than executions") if @enum.max > @executions
+    @failures = (0..(@executions)).to_a.sample(@enum.max).sort
   end
 end
